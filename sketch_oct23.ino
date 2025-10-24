959e4804243b213ca17106f49ae37b8253d11a29
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-10-24 18:59:40 +0200

    Add files via upload

--- sketch_oct23.ino
+++ sketch_oct23.ino
@@ -99,14 +99,14 @@ const int WAIT_TIME_BUTTON = 300;
 
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   Wire.begin();
   if (!rtc.begin()) {
-    Serial.println("RTC not found");
+    //Serial.println("RTC not found");
     while (1);
   }
   if (!bme.begin(0x76)) {
-    Serial.println("BME280 not found");
+    //Serial.println("BME280 not found");
     while (1);
   }
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -121,7 +121,7 @@ void setup() {
 
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)) {
-    Serial.println(F("SSD1306 nicht gefunden!"));
+    //Serial.println(F("SSD1306 nicht gefunden!"));
     for (;;); // Stoppt hier
   }
   display.clearDisplay();
@@ -201,7 +201,23 @@ void loop() {
   humids[meassure_counter] = bme.readHumidity();
   pressures[meassure_counter] = bme.readPressure() / 100.0F; // in hPa
 
-  meassure_counter = meassure_counter < 4 ? meassure_counter + 1 : 0;
+  if (meassure_counter<4){
+    meassure_counter + 1;
+  } else {
+    // Mittelwerte bilden
+    T = H = P = 0;
+    for (int i = 0; i < measurements; ++i) {
+      T += temps[i];
+      H += humids[i];
+      P += pressures[i];
+    }
+    T /= measurements; H /= measurements; P /= measurements;
+    meassure_counter = 0;
+  }
+
+
+
+ 
 
   //  if (millis() - timer >= WAIT_TIME) {
   //    displaymode = displaymode <6? displaymode+1 : 0;
@@ -293,15 +309,32 @@ void loop() {
       }
 
       float max_temp = 0.0f;
+      float min_temp = 50.0f;
+      float durchschnitt = 0.f;
+      int divisor =0;
       for (int i = 0; i<array_len; ++i){
-        if (temp_messungen[i]>max_temp) max_temp=temp_messungen[i];
+        float current_temp_mess = temp_messungen[i];
+        if (current_temp_mess==-1) continue;
+        durchschnitt+=current_temp_mess;
+        divisor++;
+        if (current_temp_mess>max_temp) max_temp=current_temp_mess;
+        if (current_temp_mess<min_temp) min_temp=current_temp_mess; 
       }
+      durchschnitt/=divisor;
 
       
       
       display.setCursor(xMax+5, 0);
       display.print(max_temp, 1);
       display.print(" C");
+    
+      display.setCursor(xMax+5, 12);
+      display.print(durchschnitt,1);
+      display.println(" C");
+      
+      display.setCursor(xMax+5, 25);
+      display.print(min_temp,1);
+      display.println(" C");
       
       
       break;
@@ -349,7 +382,7 @@ void loop() {
       display.println(" %");
 
       display.setCursor(xMax+5, 12);
-      display.print(H,1);
+      display.print((hum_max+hum_min)/2,1);
       display.println(" %");
             
       display.setCursor(xMax+5, 25);
@@ -382,6 +415,7 @@ void loop() {
       }
       float baro_step = (pres_max-pres_min)/(SCREEN_HEIGHT - 1);
       if (baro_step <= 0) baro_step = 1.0f;
+      if (baro_step <= 0.5) baro_step = 0.5f;
 
       for (int i =0; i<array_len; ++i){
         int idx = (start + i) % array_len;
@@ -400,7 +434,7 @@ void loop() {
       display.println("hPa");
       
       display.setCursor(xMax+5, 12);
-      display.print(P,1);
+      display.print((pres_max+pres_min)/2,1);
       display.println("hPa");
       
       display.setCursor(xMax+5, 25);
@@ -419,39 +453,42 @@ void loop() {
       button_timer = millis();
       timer = millis();
     }
+    if (digitalRead(3)){
+      displaymode = displaymode >0 ? displaymode - 1 : 4;
+      button_timer = millis();
+      timer = millis();
+    }
+    // Nur zur vollen Stunde ins Array (deine Vorgabe bleibt)
+    DateTime now_tmp = rtc.now();
+    if ((int)now_tmp.hour() != old_hour) {
+      old_hour = (int)now_tmp.hour();
+      temp_messungen[old_hour]  = T;
+      humid_messungen[old_hour] = H;
+      baro_messungen[old_hour]  = P;
+    }
+    
   }
  
 
 
-  if (millis() - timer < WAIT_TIME) return;
- 
-  displaymode = displaymode < 4 ? displaymode + 1 : 0;
-  //Serial.print("Displaymode:\t");
-  //Serial.println(displaymode);
+  if (millis() - timer > WAIT_TIME){
+    displaymode = displaymode < 4 ? displaymode + 1 : 0;
+    //Serial.print("Displaymode:\t");
+    //Serial.println(displaymode);
+
   
-  // Mittelwerte neu bilden (wie bei dir)
-  T = H = P = 0;
-  for (int i = 0; i < measurements; ++i) {
-    T += temps[i];
-    H += humids[i];
-    P += pressures[i];
+  
+  
+  
+    timer = millis();
   }
-  T /= measurements; H /= measurements; P /= measurements;
+ 
 
-
-  // Nur zur vollen Stunde ins Array (deine Vorgabe bleibt)
-  DateTime now_tmp = rtc.now();
-  if ((int)now_tmp.hour() != old_hour) {
-    old_hour = (int)now_tmp.hour();
-    temp_messungen[old_hour]  = T;
-    humid_messungen[old_hour] = H;
-    baro_messungen[old_hour]  = P;
-  }
+  
 
 
 
-
-  timer = millis();
+  
 
 
 
